a206df66365ef75b264097d7a9028c8faf106836
Cody Kurpanek <codykurpanek@gmail.com>
2023-08-08 01:37:52 -0700

    Large changes: Refactorized, added arrow buttons and textbox to visualize a  specific file, and changed to image triplets, but s1 and s2_cloudy don't work as intended

--- visualize_sentinel_data/main.py
+++ visualize_sentinel_data/main.py
@@ -1,13 +1,18 @@
-from view_sentinel import view_triplet
+import matplotlib.pyplot as plt
+import view_sentinel as vs
+import os
 
 filepath = "ROIs1158_spring_15_p30.tif"
-view_triplet(filepath)
+# rgb_image = vs.get_rgb(filepath)
+
+# fig, ax = plt.subplots(ncols=3, nrows=1)
+# vs.display_rgb_image(rgb_image, fig, ax[0])
 
 
+test = vs.view_sentinel_2("", os.listdir("s2_cloudy"))
+# test.display_images(35)
 
 
-
-
-
+# use gdalinfo to see metadata about an image
 
 
